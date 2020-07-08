0fcd2c13e9ff14a7048f6e1b8f3ab7b6509b2e4b
Pietro Moro <33845712+pietromoro@users.noreply.github.com>
2020-07-08 17:18:21 +0200

    Update Validator

--- lib/activerse/validator.rb
+++ lib/activerse/validator.rb
@@ -6,19 +6,7 @@ module Activerse
     end
 
     def check_validity
-      raise "Attribute 'name' not found" if @input.dig(:name).nil?
-      raise "Attribute 'pages' not found" if @input.dig(:pages).nil?
-      raise "Attribute 'pages' must be an Array" unless @input.dig(:pages).is_a?(Array)
-      @input.dig(:pages).each do |page|
-        raise "Each page must be contain a name and a fields attribute" unless page.is_a?(Hash)
-        raise "Attribute 'name' not found for page" if page.dig(:name).nil?
-        raise "Attribute 'name' should be a String" unless page.dig(:name).is_a?(String)
-        raise "Attribute 'fields' not found for page #{page.dig(:name)}" if page.dig(:fields).nil?
-        raise "Attribute 'fields' for page #{page.dig(:name)} should be an Array" unless page.dig(:fields).is_a?(Array)
-        page.dig(:fields).each do |field|
-          raise "Attribute should be an Hash" unless field.is_a?(Hash)
-        end
-      end
+      # TODO: validate structure file schema
     end
 
     def name
