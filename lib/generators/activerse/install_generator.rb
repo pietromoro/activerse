3927358ef6e03e7d291b834c15ad3fd48428d101
Pietro Moro <pietro@pietromoro.dev>
2021-01-24 21:43:04 +0100

    Ensure credentials files exist

--- lib/generators/activerse/install_generator.rb
+++ lib/generators/activerse/install_generator.rb
@@ -11,6 +11,13 @@ module Activerse
         template "activerse.rb", "config/initializers/activerse.rb"
       end
 
+      def ensure_credentials_files
+        key_path = Rails.application.config.credentials.key_path
+        credentials_path = Rails.application.config.credentials.content_path
+        encryption_key_file_generator.add_key_file(key_path) unless File.exists? key_path
+        credentials_generator.add_credentials_file_silently unless File.exists? credentials_path
+      end
+
       def update_git
         return unless Dir.exists? ".git"
         if File.exists? "config/credentials.enc.yml"
@@ -26,6 +33,21 @@ module Activerse
 
       def show_readme
         readme "README" if behavior == :invoke
+      end
+    
+    private
+      def encryption_key_file_generator
+        require "rails/generators"
+        require "rails/generators/rails/encryption_key_file/encryption_key_file_generator"
+
+        Rails::Generators::EncryptionKeyFileGenerator.new
+      end
+
+      def credentials_generator
+        require "rails/generators"
+        require "rails/generators/rails/credentials/credentials_generator"
+
+        Rails::Generators::CredentialsGenerator.new
       end
     end
   end
