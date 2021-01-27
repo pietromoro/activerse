5657b4ec894cc24aeba212a3a3a2eabcbd85b1c1
Pietro Moro <pietro@pietromoro.dev>
2021-01-27 01:16:43 +0100

    Use instance variables

--- lib/activerse/dsl.rb
+++ lib/activerse/dsl.rb
@@ -1,8 +1,8 @@
 module Activerse
   class Dsl
     def initialize(hash = {})
-      @@credentials = hash
-      @@current_keys = []
+      @credentials = hash
+      @current_keys = []
     end
 
   private
@@ -10,23 +10,21 @@ module Activerse
       guess = `tput cols`.to_i
       guess == 0 ? 80 : guess
       puts " #{title.to_s} ".center(guess, '=')
-      @@current_keys.push sym.nil? ? title.to_sym : sym.to_sym
+      @current_keys.push sym.nil? ? title.to_sym : sym.to_sym
       instance_eval(&block)
-      @@current_keys.pop
+      @current_keys.pop
       puts '-' * guess
     end
 
     def subsection name, &block
-      @@current_keys.push name.to_sym
+      @current_keys.push name.to_sym
       instance_eval(&block)
-      @@current_keys.pop
+      @current_keys.pop
     end
 
     def set key, to: nil
-      keys = @@current_keys.push(key.to_sym)
-      last = keys.inject(@@credentials) { |structure, key| structure[key] ||= {} }
-      last[keys.last] = to
-      @@current_keys.pop
+      last = @current_keys.inject(@credentials) { |structure, key| structure[key] ||= {} }
+      last[key.to_sym] = to
     end
  
     def ask question, echo: true
