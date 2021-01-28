c1fb3f95eaece2b2459ff9efab5e32f3265f935c
Pietro Moro <pietromoro@programmer.net>
2021-01-28 14:05:01 +0100

    Prefer YAML.safe_load

--- lib/generators/activerse/fill_generator.rb
+++ lib/generators/activerse/fill_generator.rb
@@ -6,7 +6,7 @@ module Activerse
       desc "Fill out the credentials file and run startup code"
 
       def fill_out
-        current_credentials = YAML.load(Rails.application.credentials.read)
+        current_credentials = YAML.safe_load(Rails.application.credentials.read)
         current_credentials.symbolize_keys!
         dsl = Activerse::Dsl.new(current_credentials)
         dsl.instance_eval(&Activerse::Configs.internal_structure)
